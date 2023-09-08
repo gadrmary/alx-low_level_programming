561546f2218be3d6bc5a4f2057104a38f322b99a
gadrmary <gadrmary2@gmail.com>
2023-09-07 18:25:15 -0700

    size

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints "with proper grammar, but the outcome is a biece of art,"
+ followed newline
+ * Return:0;
+*/
+int main(void)
+{
+        printf("Print with proper grammar, but the outcome is a biece of art,\n");
+        return (0);
+}
